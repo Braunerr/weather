2b512e9fb27ad3577dfc8c4c10c819f6b4b03cd1
Braunerr <mathn519@gmail.com>
2021-12-10 10:02:00 +0100

    responsive v2

--- public/main.js
+++ public/main.js
@@ -5,12 +5,13 @@ const svgMap = d3.select("#world"),
     heightMap = +svgMap.attr("height");
 
 // Map and projection
+svgMap.call(responsivefy)
 const path = d3.geoPath();
 const projection = d3
     .geoMercator()
-    .scale(130)
+    .scale(125)
     .center([0, 20])
-    .translate([widthMap / 2.2, heightMap / 2]);
+    .translate([widthMap / 2, heightMap / 2])
 
 // Load external data and boot
 Promise.all([d3.json("continents.json")]).then(function (loadData) {
@@ -40,6 +41,7 @@ Promise.all([d3.json("continents.json")]).then(function (loadData) {
         .data(topo.features)
         .enter()
         .append("path")
+        .call(responsivefy)
         // draw each continent and give proper id
         .attr("d", d3.geoPath().projection(projection))
         .attr("id", function (d) {
@@ -70,15 +72,13 @@ const svg = d3
 
 // Add X axis
 const x = d3.scaleLinear().range([0, width]);
-const xAxis = d3.axisBottom().scale(x);
+const xAxis = d3.axisBottom().scale(x).tickFormat(d3.format("d"));
 svg.append("g").attr("transform", `translate(0, ${height})`).attr("class", "myXaxis").style("color", "white");
 
 // Add Y axis
 const y = d3.scaleLinear().range([height, 0]);
 const yAxis = d3.axisLeft().scale(y);
 svg.append("g").attr("class", "myYaxis").style("color", "white");
-
-let sumstat;
 
 function update(selectedVar) {
     // Parse the other Data
@@ -88,7 +88,7 @@ function update(selectedVar) {
         const data = response.data;
 
         // group the data: I want to draw one line per group
-        sumstat = d3.group(data, (d) => d.disaster_type); // nest function allows to group the calculation per level of a factor
+        const typer = d3.group(data, (d) => d.disaster_type); // nest function allows to group the calculation per level of a factor
 
         console.log(data);
 
@@ -112,7 +112,7 @@ function update(selectedVar) {
 
         // Draw the line
         svg.selectAll(".line")
-            .data(sumstat)
+            .data(typer)
             .join("path")
             .transition()
             .duration(1000)
@@ -130,7 +130,7 @@ function update(selectedVar) {
             .attr("stroke", function (d) {
                 return color(d[0]);
             })
-            .attr("stroke-width", 3);
+            .attr("stroke-width", 2.5);
     });
 }
 update("1");
@@ -175,87 +175,6 @@ function responsivefy(svg) {
     }
   }
 
-/*
-    // The chart
-// set the dimensions and margins of the graph
-const margin = { top: 30, right: 30, bottom: 70, left: 60 },
-chartWidth = 500 - margin.left - margin.right,
-    chartHeight = 500 - margin.top - margin.bottom;
-    
-    // append the svg object to the body of the page
-    const svg = d3
-    .select("#chart")
-    .append("svg")
-    .attr("width", chartWidth + margin.left + margin.right)
-    .attr("height", chartHeight + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
-    
-    // Initialize the X axis
-    const x = d3.scaleBand().range([0, chartWidth]).padding(0.2);
-    const xAxis = svg.append("g").attr("transform", `translate(0,${chartHeight})`).style("color", "white");
-    
-// Initialize the Y axis
-const y = d3.scaleLinear().range([chartHeight, 0]);
-const yAxis = svg.append("g").attr("class", "myYaxis").style("color", "white");
-
-// A function that create / update the plot for a given variable:
-function update(selectedVar) {
-    // Parse data
-    d3.json("/data", {
-        method: "POST",
-    }).then(function (response) {
-        const data = response.data; // Hent data ud af response
-        console.log(data);
-        d3.select("#deaths").select("span").attr("id", "deathsSpan").text(data[selectedVar].deaths).style("font-size", "45px");
-
-        d3.select("#damages")
-            .select("span")
-            .attr("id", "damagesSpan")
-            .text(data[selectedVar].damages + "$")
-            .style("font-size", "45px");
-    });
-
-    // Parse the other Data
-    d3.json("/type", {
-        method: "POST",
-    }).then(function (response) {
-        const data = response.data;
-
-        let dataArray = [];
-
-        console.log(data)
-        for (let i = 0; i < 5; i++) {
-            dataArray.push(data[[selectedVar] * 5 + i]);
-        }
-        console.log(dataArray);
-        // X axis
-        x.domain(dataArray.map((d) => d.disaster_type));
-        xAxis.transition().duration(1000).call(d3.axisBottom(x)).selectAll("text").attr("transform", "translate(-10,0)rotate(-45)").style("text-anchor", "end").style("color", "white");
-
-        // Add Y axis
-        y.domain([0, d3.max(dataArray, (d) => +d.count)]);
-        yAxis.transition().duration(1000).call(d3.axisLeft(y)).call(d3.axisLeft(y)).selectAll("text").style("color", "white");
-
-        // variable u: add data to existing bars
-        const u = svg.selectAll("rect").data(dataArray);
-
-        // update bars
-        u.join("rect")
-            .transition()
-            .duration(1000)
-            .attr("x", (d) => x(d.disaster_type))
-            .attr("y", (d) => y(d.count))
-            .attr("width", x.bandwidth())
-            .attr("height", (d) => chartHeight - y(d.count))
-            .attr("fill", "#cf8838")
-            .attr("opacity", "1");
-    });
-}
-
-// Initialize plot
-update("0");
-*/
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     anchor.addEventListener("click", function (e) {
         e.preventDefault();
